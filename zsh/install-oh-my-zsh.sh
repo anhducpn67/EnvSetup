4f714aa8b4aa91b2faa2da580683f80f48dc30f7
Nguyen Tran Anh Duc <ducnta3@ghtk.co>
2022-12-28 16:32:38 +0700

    Update install-oh-my-zsh.sh

--- zsh/install-oh-my-zsh.sh
+++ zsh/install-oh-my-zsh.sh
@@ -5,7 +5,7 @@ set -e
 # usage
 usage() {
     cat <<- _EOF_
-Download oh-my-zsh installtion script and install.
+Install oh-my-zsh and some useful plugins.
 
 OPTION:
         --download-only     Only download script
@@ -35,12 +35,14 @@ parse_args $@
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh\
     -qO install.sh
 
-# install oh-my-zsh and zsh syntax highlighting plugin
+# install oh-my-zsh and some useful plugins
 if [[ -z $INSTALL ]]; then
     chmod u+x install.sh
     ./install.sh --unattended
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --all
     rm install.sh
 fi
 
